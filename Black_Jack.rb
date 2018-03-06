5d14e6ccd850207f15958ee4fd1fb588d39e1ae0
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-03-06 09:17:36 -0700

    probably should have donethis a bit ago.  i refactored the casino and did a lot of other goodies

--- Black_Jack.rb
+++ Black_Jack.rb
@@ -1,90 +1,91 @@
-@deck = ["Ace",2, 3,4,5,6,7, 8, 9,10,"Jack","Queen","King","Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"]
+class BlackJack < Game
 
+  def play
 
-def round
-
-  # playerHand = ["Ace"]
-  playerHand = [] << @deck.sample
-  dealerHand = [] << @deck.sample
-  p "Can you get closer than the dealer to 21 without going over?"
-  p "First off, how much would you like to bet?"
-  playerBet = gets.chomp!
-  p "Fantastic. You are dealt #{playerHand.first}."
-  p "You can either hit, or you can stay"
-  p "(hit/stay)"
-  playerAction = gets.chomp!
-
-  while playerAction == "hit"
-    playerHand = playerHand << @deck.sample
-
-    dealerHand = dealerHand << @deck.sample
-    puts " Your hand is how #{playerHand}. (hit/stay)"
+    # playerHand = ["Ace"]
+    playerHand = [] << @deck.sample
+    dealerHand = [] << @deck.sample
+    p "Can you get closer than the dealer to 21 without going over?"
+    p "First off, how much would you like to bet?"
+    playerBet = gets.chomp!
+    p "Fantastic. You are dealt #{playerHand.first}."
+    p "You can either hit, or you can stay"
+    p "(hit/stay)"
     playerAction = gets.chomp!
-  end
 
-  while playerAction == "stay"
-  
-    dealerTotal = dealerHand.map { |x| x == "King" ? 10 : x }
-    dealerTotal = dealerTotal.map { |x| x == "Queen" ? 10 : x }
-    dealerTotal = dealerTotal.map { |x| x == "Jack" ? 10 : x }
-    playerTotal = playerHand.map { |x| x == "King" ? 10 : x }
-    playerTotal = playerTotal.map { |x| x == "Queen" ? 10 : x }
-    playerTotal = playerTotal.map { |x| x == "Jack" ? 10 : x }
+    while playerAction == "hit"
+      playerHand = playerHand << @deck.sample
+
+      dealerHand = dealerHand << @deck.sample
+      puts " Your hand is how #{playerHand}. (hit/stay)"
+      playerAction = gets.chomp!
+    end
+
+    while playerAction == "stay"
+    
+      dealerTotal = dealerHand.map { |x| x == "King" ? 10 : x }
+      dealerTotal = dealerTotal.map { |x| x == "Queen" ? 10 : x }
+      dealerTotal = dealerTotal.map { |x| x == "Jack" ? 10 : x }
+      playerTotal = playerHand.map { |x| x == "King" ? 10 : x }
+      playerTotal = playerTotal.map { |x| x == "Queen" ? 10 : x }
+      playerTotal = playerTotal.map { |x| x == "Jack" ? 10 : x }
 
 
-    if playerTotal.include? "Ace"
-      playerTotal.delete("Ace")
-      if playerTotal.sum + 11 > 21
-        playerTotal << 1
-      elsif playerTotal.sum + 11 <= 21
-        playerTotal << 11
+      if playerTotal.include? "Ace"
+        playerTotal.delete("Ace")
+        if playerTotal.sum + 11 > 21
+          playerTotal << 1
+        elsif playerTotal.sum + 11 <= 21
+          playerTotal << 11
+        end
+        else
+          p "well poop"
+       end 
+            
+      if dealerTotal.include? "Ace"
+        dealerTotal.delete("Ace")
+        if dealerTotal.sum + 11 > 21
+          dealerTotal << 1
+        elsif dealerTotal.sum + 11 <= 21
+          dealerTotal << 11
+        end
+        else
+          p "well poop"
+       end 
+
+
+        dealerTotal = dealerTotal.inject(:+)
+        playerTotal = playerTotal.inject(:+)
+
+        
+        puts "Okay great!  Now lets see who wins.  You had #{playerHand} for a total of #{playerTotal}."
+        puts "Press return to contunue"
+        filler = gets.chomp!
+        puts "The dealer had #{dealerHand} for a total of #{dealerTotal}."
+        puts "Press return to contunue"
+        filler = gets.chomp!
+        playerWinnings = playerBet.to_i * 2
+
+        if dealerTotal > 21 && playerTotal > 21
+          puts "You both bust.  You get your bet of #{playerBet} back"
+        elsif dealerTotal > 21 && playerTotal < 22
+          puts "The dealer busted! You recieve #{playerWinnings}"
+        elsif dealerTotal <22 && playerTotal > 21
+          puts "You busted and lose your bet of #{playerBet}."
+        elsif dealerTotal > playerTotal
+          puts "The Dealer wins. You lose your bet of #{playerBet}."
+        elsif playerTotal > dealerTotal
+          puts "You win!  You are awarded #{playerWinnings}!"
+        else 
+          puts "Hopefully you dont see this because if you do then that means its broke and being broke is no fun :("
+        end 
+
+        break
+
       end
-      else
-        p "well poop"
-     end 
-          
-    if dealerTotal.include? "Ace"
-      dealerTotal.delete("Ace")
-      if dealerTotal.sum + 11 > 21
-        dealerTotal << 1
-      elsif dealerTotal.sum + 11 <= 21
-        dealerTotal << 11
-      end
-      else
-        p "well poop"
-     end 
-
-
-      dealerTotal = dealerTotal.inject(:+)
-      playerTotal = playerTotal.inject(:+)
-
-      
-      puts "Okay great!  Now lets see who wins.  You had #{playerHand} for a total of #{playerTotal}."
-      puts "Press return to contunue"
-      filler = gets.chomp!
-      puts "The dealer had #{dealerHand} for a total of #{dealerTotal}."
-      puts "Press return to contunue"
-      filler = gets.chomp!
-      playerWinnings = playerBet.to_i * 2
-
-      if dealerTotal > 21 && playerTotal > 21
-        puts "You both bust.  You get your bet of #{playerBet} back"
-      elsif dealerTotal > 21 && playerTotal < 22
-        puts "The dealer busted! You recieve #{playerWinnings}"
-      elsif dealerTotal <22 && playerTotal > 21
-        puts "You busted and lose your bet of #{playerBet}."
-      elsif dealerTotal > playerTotal
-        puts "The Dealer wins. You lose your bet of #{playerBet}."
-      elsif playerTotal > dealerTotal
-        puts "You win!  You are awarded #{playerWinnings}!"
-      else 
-        puts "Hopefully you dont see this because if you do then that means its broke and being broke is no fun :("
-      end 
-
-      break
 
     end
 
-  end
+end
 
   round
--- casinoOperator.rb
+++ casinoOperator.rb
@@ -1,8 +1,10 @@
 # The operator for the casino and all its related functions:
-# welcome, wallet, games directory, deck of cards
+# welcome, wallet, games directory
 
 class Casino
   def initialize
+    @myWallet = Wallet.new
+    @game = Game.new
     welcome
   end
 
@@ -13,19 +15,56 @@ class Casino
     welcomeAction = gets.chomp!
 
     if welcomeAction == "chips"
-      # p "Welcome to the chips counter! You currently have #{chips} chips valued at #{chips * 2} dollars."
-      p "Your chips are very important here at ABL Casino, here any game requires chips to play.  If you lose a game, you will lose your chips, if you tie a game you get your chips back, but if you win your chips will double!"
+      p "Welcome to the chips counter! Your chips are very important here at ABL Casino, here any game requires chips to play.  If you lose a game, you will lose your chips, if you tie a game you get your chips back, but if you win your chips will double!"
       chipsCounter
 
     elsif welcomeAction == "games"
-      p "you are in games"
+      gamesCounter
 
-    else welcomeAction == "exit"
-      p "Goodbye!!"
+    elsif welcomeAction == "exit"
+      p "Goodbye!! Come again soon!!"
+
+    else
+      puts "sorry, that input is invalid. please try again"
     end
   end
 
+  def gamesCounter
+    puts "you are in the game counter"
+    if @game.winLoseStatus == -1
+      puts "which game would you like to play?"
+      puts "(game / <other games to be added)"
+      selectgame = gets.chomp!
+      puts "please place your bet"
+      @bet = gets.chomp!.to_i
+      p @bet
+      p selectgame
+      if @bet <= @myWallet.chips
+        puts "Your bet of #{@bet} chips has been recorded. Enjoy your game!"
+        if selectgame == "game"
+          puts "you are inside game"
+        @game.play
+        gamesCounter
+        else 
+          puts "that input is invalid"
+          gamesCounter
+        end
+      else
+        puts "sorry, you only have #{@myWallet.chips}. Your bet cannot exceed the amount of chips owned"
+        gamesCounter
+      end 
+    elsif @game.winLoseStatus == 0
+      p @myWallet.chips
+      @myWallet.addChips(@bet * 2)
+      p @myWallet.chips
+
+        
+    end
+
+  end
+
   def chipsCounter
+    p "You currently have #{@myWallet.chips} chips valued at #{@myWallet.chips * 2} dollars."
     
     p "Would you like to buy chips, sell chips, or leave?"
     p "(buy / sell / leave)"
@@ -33,10 +72,17 @@ class Casino
 
     if ccaction == "buy"
       puts "how many chips would you like to buy?"
-      purchase = gets.chomp!
-      @@wallet.addChips(purchase)
+      purchase = gets.chomp!.to_i
+      @myWallet.addChips(purchase)
+      chipsCounter
+
 
     elsif ccaction == "sell"
+      puts "how many chips would you like to sell?"
+      sell = gets.chomp!.to_i
+      @myWallet.subChips(sell)
+      puts "you sold #{sell} chips for #{sell * 2} dollars"
+      chipsCounter
 
     elsif ccaction == "leave"
       welcome
@@ -51,25 +97,64 @@ class Casino
 end
 
 class Wallet
-  # attr_accessor :chips
+  attr_accessor :chips
   def initialize
-    @chips = 0
+    @chips = 10
   end
 
   def addChips(amount)
-
-    puts @chips
-    @chips  = @chips + amount.to_i
-    puts @chips
-    @@casino.chipsCounter
+    @chips += amount
+    p "you are here"
   end
 
-  def self.takeChips(amount)
-    @chips = @chips + amount
+  def subChips(amount)
+    @chips -= amount
   end
 end
 
+class Game
+  attr_accessor :winLoseStatus
 
-@@wallet = Wallet.new
-@@casino = Casino.new
-Casino.new
+  def initialize
+    @deck = ["Ace",2, 3,4,5,6,7, 8, 9,10,"Jack","Queen","King","Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"]
+    @bet = 0
+    @winLoseStatus = -1
+
+  end
+
+  def play
+    playaction = gets.chomp!
+
+    if playaction == "0"
+      puts "this is win"
+      win
+    elsif playaction == "1"
+      puts "this is lose"
+      lose
+    elsif playaction == "2"
+      puts "this is tie"
+      tie
+    else
+      puts "this is else"
+      play
+    end    
+  end
+
+  def win
+    puts "you won!!"
+    @winLoseStatus = 0
+    # Casino.gamesCounter
+  end
+
+  def lose
+    puts "you lost"
+    @winLoseStatus = 1
+  end
+
+  def tie
+    puts "you tied"
+    @winLoseStatus = 2
+  end
+end
+
+@casino = Casino.new
